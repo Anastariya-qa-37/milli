1c8de0f2b825fa63a2523b442f1a76c142b00719
Anastasya <blum1098@yandex.ru>
2022-08-21 23:11:02 +0300

    Add file

---
+++ scr/main/java/Milli.java
@@ -0,0 +1,9 @@
+public class Start {
+    public static void main (String[] args) {
+        int price = 5300;
+        int w = 20;
+        int mile = price/w ;
+
+        System.out.println(mile);
+    }
+}
