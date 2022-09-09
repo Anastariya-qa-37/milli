d98e0223538e68bdae88e3e52cfb8fc070236e9b
Anastasya <blum1098@yandex.ru>
2022-09-09 14:15:26 +0300

    rename

--- scr/main/java/Milli.java
+++ scr/main/java/Milli.java
@@ -1,8 +1,8 @@
 public class Start {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int price = 5300;
-        int w = 20;
-        int mile = price/w ;
+        int cost_mile = 20;
+        int mile = price / cost_mile;
 
         System.out.println(mile);
     }
